0f453fc6b325899460817c15ea8d799bfcff7e0c
sunhil2007 <59070685+sunhil2007@users.noreply.github.com>
2020-03-06 17:24:06 -0600

    Update k8-createinfra.sh

--- k8-createinfra.sh
+++ k8-createinfra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/echo "i am in cloud shell"
 gcloud --version
-gcloud container --project "devopssunil" clusters create "k8-sv-cluster1" --zone "us-central1-c" --cluster-version "1.14.10-gke.17" --machine-type "g1-small" --image-type "COS" --num-nodes "3"
+gcloud get nodes 
 
 
